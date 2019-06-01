22f70e7cbabee1971a11923aa42cf845b2db6631
jyin25 <34949979+jyin25@users.noreply.github.com>
2019-06-01 11:17:17 -0700

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-const config = {
-  yelpApiKey: 'rKBleUfKJSjnRt4heyZ3KL_j1BOMSuKlZcev_lm1RlCDi9r3zXh-VVzOLAyHiUi3X8Kp_m4mqMtunsQwKeU6fIUJu1EogqIsZqCZP1kYYgeEqS59f1V8O3UUaKHpXHYx',
-  weatherApiKey: 'b1168a00b5924ba68e2200452192505'
-}
