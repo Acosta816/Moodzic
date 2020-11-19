70d3cd4cd3c27c5069f5ef25c95102315f02529a
David Acosta <45275778+Acosta816@users.noreply.github.com>
2020-11-18 23:47:16 -0800

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-const config = {
-  yelpApiKey: 'rKBleUfKJSjnRt4heyZ3KL_j1BOMSuKlZcev_lm1RlCDi9r3zXh-VVzOLAyHiUi3X8Kp_m4mqMtunsQwKeU6fIUJu1EogqIsZqCZP1kYYgeEqS59f1V8O3UUaKHpXHYx',
-  weatherApiKey: '5e5d0335f233069de31ba5c673292ecd' //David's api key
-}
